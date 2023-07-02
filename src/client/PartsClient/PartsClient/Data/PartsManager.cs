962a0969e93c2d42591d6e3ff308c508e423b44d
Marco von Ballmoos <marco@earthli.com>
2023-07-02 21:41:20 +0200

    Add Authentication/authorization and data-retrieval

--- src/client/PartsClient/PartsClient/Data/PartsManager.cs
+++ src/client/PartsClient/PartsClient/Data/PartsManager.cs
@@ -13,19 +13,40 @@ namespace PartsClient.Data
     public static class PartsManager
     {
         // TODO: Add fields for BaseAddress, Url, and authorizationKey
-        static readonly string BaseAddress = "URL GOES HERE";
+        static readonly string BaseAddress = "https://mslearnpartsserver91295563.azurewebsites.net";
         static readonly string Url = $"{BaseAddress}/api/";
+        private static string authorizationKey;
 
         static HttpClient client;
 
         private static async Task<HttpClient> GetClient()
         {
-            throw new NotImplementedException();
+            if (client != null)
+                return client;
+
+            client = new HttpClient();
+
+            if (string.IsNullOrEmpty(authorizationKey))
+            {
+                authorizationKey = await client.GetStringAsync($"{Url}login");
+                authorizationKey = JsonConvert.DeserializeObject<string>(authorizationKey);
+            }
+
+            client.DefaultRequestHeaders.Add("Authorization", authorizationKey);
+            client.DefaultRequestHeaders.Add("Accept", "application/json");
+
+            return client;
         }
 
         public static async Task<IEnumerable<Part>> GetAll()
         {
-            throw new NotImplementedException();                
+            if (Connectivity.Current.NetworkAccess != NetworkAccess.Internet)
+                return new List<Part>();
+                
+            HttpClient client = await GetClient();
+            string result = await client.GetStringAsync($"{Url}parts");
+
+            return JsonConvert.DeserializeObject<List<Part>>(result);
         }
 
         public static async Task<Part> Add(string partName, string supplier, string partType)
