916feb9cbb7e1c3ead35cf5d6f1476a05782c80b
Marco von Ballmoos <marco@earthli.com>
2023-07-03 17:42:33 +0200

    Update the sandbox URL

--- src/client/PartsClient/PartsClient/Data/PartsManager.cs
+++ src/client/PartsClient/PartsClient/Data/PartsManager.cs
@@ -13,7 +13,7 @@ namespace PartsClient.Data
     public static class PartsManager
     {
         // TODO: Add fields for BaseAddress, Url, and authorizationKey
-        static readonly string BaseAddress = "https://mslearnpartsserver91295563.azurewebsites.net";
+        static readonly string BaseAddress = "https://mslearnpartsserver1524324216.azurewebsites.net";
         static readonly string Url = $"{BaseAddress}/api/";
         private static string authorizationKey;
 
@@ -51,17 +51,52 @@ namespace PartsClient.Data
 
         public static async Task<Part> Add(string partName, string supplier, string partType)
         {
-            throw new NotImplementedException();
+            if (Connectivity.Current.NetworkAccess != NetworkAccess.Internet)
+                return new Part();
+
+            Part part = new Part()
+            {
+                PartName = partName,
+                Suppliers = new List<string>(new[] { supplier }),
+                PartID = string.Empty,
+                PartType = partType,
+                PartAvailableDate = DateTime.Now.Date
+            };
+
+            var msg = new HttpRequestMessage(HttpMethod.Post, $"{Url}parts");
+
+            msg.Content = JsonContent.Create<Part>(part);
+
+            var response = await client.SendAsync(msg);
+            response.EnsureSuccessStatusCode();
+
+            var returnedJson = await response.Content.ReadAsStringAsync();
+
+            var insertedPart = JsonConvert.DeserializeObject<Part>(returnedJson);
+
+            return insertedPart;
         }
 
         public static async Task Update(Part part)
         {
-            throw new NotImplementedException();
+            if (Connectivity.Current.NetworkAccess != NetworkAccess.Internet)
+                return;
+
+            HttpRequestMessage msg = new(HttpMethod.Put, $"{Url}parts/{part.PartID}");
+            msg.Content = JsonContent.Create<Part>(part);
+            HttpClient client = await GetClient();
+            var response = await client.SendAsync(msg);
+            response.EnsureSuccessStatusCode();
         }
 
         public static async Task Delete(string partID)
         {
-            throw new NotImplementedException();                        
+            if (Connectivity.Current.NetworkAccess != NetworkAccess.Internet)
+                return;
+            HttpRequestMessage msg = new(HttpMethod.Delete, $"{Url}parts/{partID}");
+            HttpClient client = await GetClient();
+            var response = await client.SendAsync(msg);
+            response.EnsureSuccessStatusCode();
         }
     }
 }
